adfe0cc600bc5abb1c0b4fa7c49fc18f515ca8f9
Benn Sandoval <bennsandoval>
2015-01-05 23:22:04 -0600

    Call and send email function

---
+++ app/src/main/java/app/bennsandoval/com/woodmin/activities/OrderDetail.java
@@ -0,0 +1,7 @@
+package app.bennsandoval.com.woodmin.activities;
+
+/**
+ * Created by Mackbook on 1/5/15.
+ */
+public class OrderDetail {
+}
---
+++ app/src/main/java/app/bennsandoval/com/woodmin/interfaces/CustomerActions.java
@@ -0,0 +1,7 @@
+package app.bennsandoval.com.woodmin.interfaces;
+
+/**
+ * Created by Mackbook on 1/5/15.
+ */
+public interface CustomerActions {
+}
